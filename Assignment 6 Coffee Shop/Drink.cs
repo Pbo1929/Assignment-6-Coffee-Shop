b7403a11b4496faa3849a595f5a775453789f589
Prem Worapon Sumsai <lulztit1929@gmail.com>
2021-01-24 16:56:17 +0700

    Add project files.

---
+++ Assignment 6 Coffee Shop/Drink.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_6_Coffee_Shop
+{
+    class Drink
+    {
+        private string name;
+        private bool Sold;
+        private int price;
+
+        public static int NumberOfDrinksSold = 0;
+
+        public Drink(string name, int price)
+        {
+
+            this.name = name;
+            this.Sold = false;
+            this.price = price;
+
+            
+        }
+
+        public void BuyDrink()
+        {
+            if (Sold == false)
+            {
+                Sold = true;
+                NumberOfDrinksSold = NumberOfDrinksSold + 1;
+
+            }
+        }
+    }
+}
+
---
+++ Assignment 6 Coffee Shop/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Assignment_6_Coffee_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Drink Coffee = new Drink("Coffee", 50);
+            Drink Tea = new Drink("Tea", 45);
+            Drink Juice = new Drink("Juice", 30);
+        }
+    }
+}
